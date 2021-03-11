35a47833183b5d38d0b809c3695a2b11b2d56a8b
Bhaskar Gogoi <bhaskar.gogoi@albanero.io>
2021-03-11 08:44:10 +0100

    Changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000;
