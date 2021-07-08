8ea79956e3bc158e82e608df57177e2b9bfd756a
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2021-07-08 09:25:36 +0200

    Completed Landing page and authentication flow

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 import * as actions from '../actions';
 import { connect } from 'react-redux';
+import Landing from './Landing';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
   componentDidMount() {
@@ -19,8 +19,8 @@ class App extends Component {
         <BrowserRouter>
           <div>
             <Header />
-            <Route path='/' exact component={Landing} />
-            <Route path='/surveys' exact component={Dashboard} />
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/surveys' component={Dashboard} />
             <Route path='/surveys/new' component={SurveyNew} />
           </div>
         </BrowserRouter>
