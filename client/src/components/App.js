93e5b01356dd0595d57e50b17f1cf12c01fcf4ea
Bhaskar Gogoi <bhaskargogoi24@gmail.com>
2021-07-08 08:16:05 +0200

    Initial Redux setup and adding authReducer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
 
-const App = () => {
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
     return (
-        <div>
-            Hi There!
-        </div>
+      <div className='container'>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path='/' exact component={Landing} />
+            <Route path='/surveys' exact component={Dashboard} />
+            <Route path='/surveys/new' component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
     );
+  }
 }
 
-export default App;
+export default connect(null, actions)(App);
